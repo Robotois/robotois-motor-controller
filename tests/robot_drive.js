47e050a52ce6f326eb08a073bf8124b4c70cb337
Armando Gonzalez <iam@armando.mx>
2017-03-03 16:39:14 -0600

    Added linter support

--- tests/robot_drive.js
+++ tests/robot_drive.js
@@ -1,28 +1,30 @@
-var _motors = require('../index.js'),
-  motors = new _motors();
+const Motors = require('../index.js');
 
-var pwm = -50,sum = 5;
+const motors = new Motors();
 
-setInterval(()=> {
+let pwm = -50;
+let sum = 5;
+
+setInterval(() => {
   // The drive PWM considers that the motor 1 is moving the right side wheel of a robot,
   // therefore the motor 2 is moving the left side wheel of the robot.
-  motors.drivePWM(pwm,pwm);
+  motors.drivePWM(pwm, pwm);
 
-  if(pwm == 50){
+  if (pwm === 50) {
     sum = -5;
   }
 
-  if(pwm == -50){
+  if (pwm === -50) {
     sum = 5;
   }
 
   pwm += sum;
-},1000);
+}, 1000);
 
-  process.on('SIGTERM', function () {
-    process.exit();
-  });
+process.on('SIGTERM', () => {
+  process.exit();
+});
 
-  process.on('SIGINT', function () {
-    process.exit();
-  });
+process.on('SIGINT', () => {
+  process.exit();
+});
--- tests/single_run.js
+++ tests/single_run.js
@@ -1,41 +1,44 @@
-var _motors = require('../index.js'),
-  motors = new _motors();
+const Motors = require('../index.js');
 
-var pwm1 = -50,sum1 = 5;
-var pwm2 = -50,sum2 = 5;
+const motors = new Motors();
+let pwm1 = -50;
+let sum1 = 5;
 
-setInterval(()=> {
+let pwm2 = -50;
+let sum2 = 5;
+
+setInterval(() => {
   motors.motor1PWM(pwm1);
 
-  if(pwm1 == 50){
+  if (pwm1 === 50) {
     sum1 = -5;
   }
 
-  if(pwm1 == -50){
+  if (pwm1 === -50) {
     sum1 = 5;
   }
 
   pwm1 += sum1;
-},1000);
+}, 1000);
 
-setInterval(()=> {
+setInterval(() => {
   motors.motor2PWM(pwm2);
 
-  if(pwm2 == 50){
+  if (pwm2 === 50) {
     sum2 = -5;
   }
 
-  if(pwm2 == -50){
+  if (pwm2 === -50) {
     sum2 = 5;
   }
 
   pwm2 += sum2;
-},1000);
+}, 1000);
 
-process.on('SIGTERM', function () {
+process.on('SIGTERM', () => {
   process.exit();
 });
 
-process.on('SIGINT', function () {
+process.on('SIGINT', () => {
   process.exit();
 });
